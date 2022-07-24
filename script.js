2ab3577c67bb79e81133d015c026f7b1da7d89a7
Ahmad Alqaisi <ahmedhamed1312@gmail.com>
2022-07-24 23:00:16 +0300

    fixing header & mobile menu

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
 const menuIcon=document.querySelector(".menuIcon");
 const overlay=document.querySelector(".overlay");
+const main=document.querySelector("html");
 
 menuIcon.addEventListener("click",function(evt){
     if(overlay.classList.contains("hidden"))
         overlay.classList.remove("hidden");
-    console.log(evt.target.alt)    
     toggleMenu(evt.target.alt)
 });
 
 function toggleMenu(menuStatus){
+    
     overlay.classList.toggle("hide");
     menuIcon.setAttribute("alt",menuStatus=="openMenu"?"closeMenu":"openMenu");
+    if(menuStatus=="openMenu"){
+        main.style.overflow="hidden";
+    }else{
+        main.style.overflowX="hidden";
+        main.style.overflowY="auto";
+    }
     if(menuStatus=="openMenu")
         return menuIcon.setAttribute("src","images/icon-close.svg");
     menuIcon.setAttribute("src","images/icon-hamburger.svg");
